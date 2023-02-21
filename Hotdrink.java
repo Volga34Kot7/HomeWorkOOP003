83f2b90478f180c913e712ed2662c2239f935fb5
Dmitrij <sdma7@mail.ru>
2023-02-21 20:11:03 +0300

    Hot

--- Hotdrink.java
+++ Hotdrink.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Hotdrink {
+public class Hotdrink {
+    private String name;
+    private Integer cost;
+    private Integer volume;
+
+    public Hotdrink(String name, Integer cost, Integer volume) {
+        this.name = name;
+        this.cost = cost;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
+    public String getInfo() {
+
+        return String.format("Name: %s, cost: %d, volume: %d", name, cost, volume);
+
+    }
 }
