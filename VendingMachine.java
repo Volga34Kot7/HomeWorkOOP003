e2c045d7c70913d206bb4a20bf1e7026e4763155
Dmitrij <sdma7@mail.ru>
2023-02-21 20:12:37 +0300

    Machine

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class VendingMachine {
+
+import java.util.ArrayList;
+import java.util.Iterator;
+public class VendingMachine {
+    public void getProduct(ArrayList<Hotdrink> hotDrinks) {
+        System.out.println("On sale: ");
+        Iterator<Hotdrink> iterator = hotDrinks.iterator();
+        while (iterator.hasNext())
+        {
+            Hotdrink item = iterator.next();
+            System.out.println(item.getName());
+        }
+    }
+
+    public void getProduct(String name, int volume, int temperature) {
+        System.out.printf("Be careful, the hottest drink: " + name + ", his temperature  " + temperature + ", beverage volume: " + volume);
+    }
 }
