b33170a30badb2e8527b0f08b35d7bb42fc58d51
Dmitrij <sdma7@mail.ru>
2023-02-21 20:11:48 +0300

    Main

--- Main.java
+++ Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Hotdrink cappuccino = new Hotdrink("cappuccino", 550, 330);
+        Hotdrink macchiato = new Hotdrink("macchiato", 750, 290);
+        Hotdrink gingerTea = new Hotdrink("ginger tea", 340, 250);
+        Hotdrink cacao = new Hotdrink("cacao", 250, 250);
+        List<Hotdrink> mainGoods = new ArrayList<>();
+        mainGoods.add(cappuccino);
+        mainGoods.add(macchiato);
+        mainGoods.add(gingerTea);
+        mainGoods.add(cacao);
+
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.getProduct((ArrayList<Hotdrink>) mainGoods);
+
+        Drinktea hotDrinkTea = new Drinktea("green tea", 500, 250, 65);
+        System.out.println(hotDrinkTea.getInfo());
+
+        vendingMachine.getProduct("green tea", 250, 65);
+
+
+
+
+    }
+
 }
